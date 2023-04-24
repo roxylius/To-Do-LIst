577c40ea3ecd2be24dceab67cfb28d44c3a081e0
roxylius <98657313+roxylius@users.noreply.github.com>
2023-04-25 01:58:24 +0530

    fix react router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Routes>
+        <Route path='/' Component={App} />
+      </Routes>
     </Router>
   </React.StrictMode>
 );
